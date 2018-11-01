016a5b2cb88f1f579d0fc597cdb31418b71657f2
Geunyoung Kim <kgysmart@gmail.com>
2018-11-01 09:01:50 -0700

    Add Buffered Channels

--- channels/main.go
+++ channels/main.go
@@ -1,10 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
+	/*  Basic channel
 	ch := make(chan string, 1)
 	ch <- "Hello"
 
 	fmt.Println(<-ch)
+	*/
+
+	phrase := "There are the times that try men's souls.\n"
+
+	words := strings.Split(phrase, " ")
+
+	ch := make(chan string, len(words))
+
+	for _, word := range words {
+		ch <- word
+	}
+
+	for i := 0; i < len(words); i++ {
+		fmt.Print(<-ch + " ")
+	}
 }
